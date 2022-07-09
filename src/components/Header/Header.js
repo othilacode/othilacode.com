ec3900de33a1e3a0d9fe5029d7fc73a8f9e9a164
quentinncl <qnicolle17@gmail.com>
2022-07-10 00:40:39 +0200

    feat: Add logo for othila

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,22 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 import { Button } from '../Button';
 
 export const Header = ({ siteTitle }) => (
   <header className="h-[80px] drop-shadow-lg">
     <div className="flex justify-between items-center px-4 py-4">
       <div className="flex justify-center">
-        <Link to="/" className="text-2xl font-bold text-zinc-900">
-          {siteTitle}
+        <Link to="/" className="p-2">
+          <StaticImage
+            src="../../images/othila.png"
+            loading="eager"
+            width={32}
+            quality={95}
+            formats={['auto', 'webp', 'avif']}
+            alt={siteTitle}
+          />
         </Link>
         <div className="mx-4 w-24 flex justify-between items-center">
           <Link to="/course" className="text-sm text-zinc-600">
