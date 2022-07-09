01fc52b94fa0eeee663512d894fd02ec55e5e5fa
quentinncl <qnicolle17@gmail.com>
2022-07-10 00:26:19 +0200

    chore: Change metadatas of website

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `OthilaCode`,
+    description: `Because I'm passionate about code. I'm a software developer and I love to create things.`,
+    author: `@othilacode`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
